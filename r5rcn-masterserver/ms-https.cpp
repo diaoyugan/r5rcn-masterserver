ae2938d653c9db6df0eb585fe73a2bfb837917bb
diaoyugan <957410390@qq.com>
2023-06-22 22:13:36 +0800

    第一个可用的正式版~~

--- r5rcn-masterserver/ms-https.cpp
+++ r5rcn-masterserver/ms-https.cpp
@@ -1,18 +1,33 @@
 ﻿// 这个版本使用SSL/TLS加密，支持https协议
-// 证书验证在217行 如果不写对域名 那么就无法运行
 
 
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <string> 
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
 #include <boost/asio/ssl.hpp>
 #include <nlohmann/json.hpp>
 
+
 namespace asio = boost::asio;
 namespace beast = boost::beast;
 namespace http = beast::http;
 using json = nlohmann::json;
+using namespace std;
+
+//设置banlist的路径
+const string banlist_file = "banlist.json"; // banlist文件的路径
+//设置证书路径
+const string crt_file = "cert.crt"; // crt文件的路径
+const string key_file = "cert.key"; // key文件的路径
+//是否启用证书验证
+const bool enable_verifi = true;
+const string cert_verifi = "ms.example.com";//证书验证的域名
+//设置监听服务器
+const string listen_address = "127.0.0.1"; // 监听地址
+const int listen_port = 443; // 监听端口
 
 // 创建一个全局变量，用于存储添加的json数据
 std::vector<json> json_data;
@@ -178,6 +193,108 @@ void handle_get_json_request(http::request<http::string_body>& req, http::respon
     res.body() = response.dump();
 }
 
+
+// 定义一个函数，用于读取banlist文件并返回一个json对象
+json read_banlist() {
+    ifstream input(banlist_file); // 打开文件
+    if (!input) { // 检查文件是否存在
+        cerr << "Error: cannot open " << banlist_file << endl;
+        exit(1);
+    }
+    json banlist; // 创建一个json对象
+    input >> banlist; // 从文件中读取json数据
+    input.close(); // 关闭文件
+    return banlist; // 返回json对象
+}
+
+
+//处理访问管理封禁系统的函数
+void handle_banlist_system_access(http::request<http::string_body>& req, http::response<http::string_body>& res) {
+    // 创建一个响应对象
+    res.version(11); // HTTP 1.1
+    res.result(http::status::ok); // 200 OK
+    res.set(http::field::content_type, "application/json"); // 设置响应内容类型为json
+    // 创建一个json对象，作为响应内容
+    json response;
+    // 从请求中获取json数据
+    json data = json::parse(req.body());
+}
+
+//检查玩家是否被封禁的函数
+void handle_banlist_check_banned(http::request<http::string_body>& req, http::response<http::string_body>& res) {
+    // 创建一个响应对象
+    res.version(11); // HTTP 1.1
+    res.result(http::status::ok); // 200 OK
+    res.set(http::field::content_type, "application/json"); // 设置响应内容类型为json
+    // 创建一个json对象，作为响应内容
+    json response;
+    // 从请求中获取json数据
+    json data = json::parse(req.body());
+    // 从json数据中获取id参数，并转换为字符串类型
+    std::string id = data.at("id").dump();
+    // 从json数据中获取id参数
+// 读取banlist.json文件，并转换为一个json对象
+    json banlist = read_banlist();
+
+    // 检查data中是否有id键，如果没有，直接返回一个错误信息给请求者
+    if (data.contains("id")) {
+        std::string id = data.at("id").dump();
+        // 判断uid参数是否在封禁列表中存在，如果存在id参数且和列表匹配 返回success为true 且banned为true 追加reason参数(从封禁列表读取)
+        if (banlist.contains(id)) { // 如果banlist中有与id相同的键
+            response["success"] = true; // 设置success为true
+            response["banned"] = true; // 设置banned为true
+            response["reason"] = banlist.at(id); // 设置reason为banlist中对应的值
+        }
+        else { // 如果banlist中没有与uid相同的键
+            response["success"] = true; // 设置success为true
+            response["banned"] = false; // 设置banned为false
+        }
+    }
+    else {
+        response["success"] = false;
+        response["error"] = "Missing player uid.";
+    }
+    // 将json对象转换为字符串，并设置为响应的body
+    res.body() = response.dump();
+    std::cout << "Send banlist check data: " << response << std::endl;
+}
+
+//批量检查玩家是否被封禁的函数
+void handle_banlist_bulkCheck(http::request<http::string_body>& req, http::response<http::string_body>& res) {
+    // 创建一个响应对象
+    res.version(11); // HTTP 1.1
+    res.result(http::status::ok); // 200 OK
+    res.set(http::field::content_type, "application/json"); // 设置响应内容类型为json
+    // 创建一个json对象，作为响应内容
+    json response;
+    // 从请求中获取json数据
+    json data = json::parse(req.body());
+    json players = data.at("players");
+    // 创建一个新的json数组，用于存储被封禁玩家信息
+    // 使用json::array()函数来指定数组类型
+    json banned_players = json::array(); 
+    // 遍历数组中的每个元素，它们应该是对象类型的值，包含id和ip两个键
+    for (auto& player : players) {
+        // 从每个对象中获取id键对应的值，并转换为字符串类型
+        std::string id = player.at("id").dump();
+        // 读取banlist.json文件，并转换为一个json对象
+        json banlist = read_banlist();
+        // 检查banlist.json对象中是否有与id相同的键，如果有，就将该键和对应的值添加到新的json数组中
+        if (banlist.contains(id)) {
+            long long id_longlong = std::stoll(id);
+            banned_players.push_back({ {"id", id_longlong}, {"reason", banlist.at(id)} });
+        }
+    }
+    // 设置success键为true，并设置bannedPlayers键为新的json数组
+    response["success"] = true;
+    response["bannedPlayers"] = banned_players;
+
+    // 将json对象转换为字符串，并设置为响应的body
+    res.body() = response.dump();
+    std::cout << "Send banlist bulkCheck data: " << response << std::endl;
+}
+
+
 //主函数
 int main() {
     try {
@@ -186,12 +303,12 @@ int main() {
         // 创建一个ssl_context对象，用于管理SSL/TLS加密相关的设置
         asio::ssl::context ssl_ctx{ asio::ssl::context::sslv23 };
         // 加载证书文件和私钥文件，这里假设文件名分别为cert.crt和cert.key
-        ssl_ctx.use_certificate_chain_file("cert.crt");
-        ssl_ctx.use_private_key_file("cert.key", asio::ssl::context::pem);
+        ssl_ctx.use_certificate_chain_file(crt_file);
+        ssl_ctx.use_private_key_file(key_file, asio::ssl::context::pem);
         // 创建一个ip地址对象，表示监听的地址，这里使用ipv4的回环地址
-        asio::ip::address address = asio::ip::make_address("127.0.0.1");
+        asio::ip::address address = asio::ip::make_address(listen_address);
         // 创建一个端口号对象，表示监听的端口，这里使用443端口
-        unsigned short port = static_cast<unsigned short>(443);
+        unsigned short port = static_cast<unsigned short>(listen_port);
         // 创建一个endpoint对象，表示监听的地址和端口的组合
         asio::ip::tcp::endpoint endpoint{ address, port };
         // 创建一个acceptor对象，用于接受客户端的连接请求
@@ -209,13 +326,18 @@ int main() {
                 asio::ssl::stream<asio::ip::tcp::socket> stream{ std::move(socket), ssl_ctx };
                 // 设置SSL/TLS握手的验证模式和回调函数，以处理可能的证书错误
 
+                if (enable_verifi){
+                               stream.set_verify_mode(asio::ssl::verify_peer);
+                               stream.set_verify_callback(asio::ssl::rfc2818_verification(cert_verifi));
+                }
+                else {
+                    //禁用证书验证
+                    stream.set_verify_mode(asio::ssl::verify_none);
+                }
 
-                //禁用证书验证 要把217行和218行注释掉 然后把下面这行取消注释
-                //stream.set_verify_mode(asio::ssl::verify_none);
 
                 //启用证书验证
-                stream.set_verify_mode(asio::ssl::verify_peer);
-                stream.set_verify_callback(asio::ssl::rfc2818_verification("ms.example.com"));
+
 
                 // 执行SSL/TLS握手操作
                 stream.handshake(asio::ssl::stream_base::server);
@@ -231,10 +353,22 @@ int main() {
                 // 判断请求的目标是否为/servers，如果是，则调用处理获取json数据的请求的函数，否则继续判断
                 if (req.target() == "/servers") {
                     handle_get_json_request(req, res);
-                    // 判断请求的目标是否为/servers/add，如果是，则调用处理添加json数据的请求的函数，否则返回404 Not Found错误
                 }
+                // 判断请求的目标是否为/servers/add，如果是，则调用处理添加json数据的请求的函数，否则继续判断
                 else if (req.target() == "/servers/add") {
                     handle_add_json_request(req, res);
+                }
+                // 判断请求的目标是否为/banlist，如果是，则调用处理访问管理封禁系统的函数，否则继续判断
+                else if (req.target() == "/banlist") {
+                    handle_banlist_system_access(req, res);
+                }
+                // 判断请求的目标是否为/banlist/isBanned，如果是，则调用处理检查玩家是否被封禁的函数，否则继续判断
+                else if (req.target() == "/banlist/isBanned") {
+                    handle_banlist_check_banned(req, res);
+                }
+                // 判断请求的目标是否为/banlist/bulkCheck，如果是，则调用处理批量检查玩家是否被封禁的函数，否则返回404 Not Found错误
+                else if (req.target() == "/banlist/bulkCheck") {
+                    handle_banlist_bulkCheck(req, res);
                 }
                 else {
                     res.version(11); // HTTP 1.1
