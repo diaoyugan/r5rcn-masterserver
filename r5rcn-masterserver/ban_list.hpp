96cc8b32f4842f24e6f3bedda707755df9e7f139
diaoyugan <957410390@qq.com>
2023-09-26 10:12:42 +0800

    整理解决方案

--- r5rcn-masterserver/ban_list.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "include\inc.hpp"
-
-extern void handle_banlist_check_banned(http::request<http::string_body>& req, http::response<http::string_body>& res);
-extern void handle_banlist_bulkCheck(http::request<http::string_body>& req, http::response<http::string_body>& res);
